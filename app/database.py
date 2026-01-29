a6b17c7a4574e1b0148367c1ca7bc2f118f31cc9
Qasim <qasimcoaxoxy@gmail.com>
2026-01-29 18:52:36 +0800

    Added Docker files

--- app/database.py
+++ app/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password} \
-@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
 from .routers import posts, users, auth, votes
-
+from contextlib import asynccontextmanager
 from . import database_model
 from .database import engine
-database_model.Base.metadata.create_all(bind=engine)
 
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     database_model.Base.metadata.create_all(bind=engine)
+#     yield
 
+# app = FastAPI(lifespan=lifespan)
 app = FastAPI()
 
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(votes.router)
+
+@app.get('/')
+def hello():
+    return {"Message": "Hello User of new reload"}
